cf1086da973b6ed6685c771de95af711ba3b62f1
Cross-companion <mickdaniels101@gmail.com>
2024-12-30 17:57:23 +0100

    Theme Color Roll on  Mobile

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,8 +7,8 @@ export default function manifest(): MetadataRoute.Manifest {
     description: "Delivery App For All",
     start_url: "/",
     display: "standalone",
-    background_color: "#ffffff",
-    theme_color: "#000000",
+    background_color: "#123524",
+    theme_color: "#123524",
     icons: [
       {
         src: "/images/icon-192x192.png",
