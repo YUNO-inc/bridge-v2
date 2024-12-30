3e04c601b2716b5a5052a9493194d03bcc2b414c
Cross-companion <mickdaniels101@gmail.com>
2024-12-30 18:15:31 +0100

    Theme Color Roll on  Mobile 2

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,23 +2,24 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Page() {
-  <Head>
-    <meta name="theme-color" content={process.env.THEME_COLOR} />
-  </Head>;
-
   return (
-    <div className="h-[100svh] w-[100vw] flex flex-col">
-      <div className="flex items-center justify-center h-[60vw] bg-phthaloGreen">
-        <div className="relative w-[45vw] aspect-[50.4/29]">
-          <Image
-            src="/images/Dark-Text-Logo-Horizontal.png"
-            alt="Large Bridge Logo (Green Color)"
-            fill
-            className="object-cover"
-          />
+    <>
+      <Head>
+        <meta name="theme-color" content={process.env.THEME_COLOR} />
+      </Head>
+      <div className="h-[100svh] w-[100vw] flex flex-col">
+        <div className="flex items-center justify-center h-[60vw] bg-phthaloGreen">
+          <div className="relative w-[45vw] aspect-[50.4/29]">
+            <Image
+              src="/images/Dark-Text-Logo-Horizontal.png"
+              alt="Large Bridge Logo (Green Color)"
+              fill
+              className="object-cover"
+            />
+          </div>
         </div>
+        <div className="grow rounded-t-2xl relative top-[-16px] bg-background"></div>
       </div>
-      <div className="grow rounded-t-2xl relative top-[-16px] bg-background"></div>
-    </div>
+    </>
   );
 }
