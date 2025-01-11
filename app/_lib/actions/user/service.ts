8759f22503f8e373ee45785a663090ed4d0bed88
Cross-companion <mickdaniels101@gmail.com>
2025-01-11 13:09:43 +0100

    SignIn and NextAuth Configs 9

--- app/_lib/actions/user/service.ts
+++ app/_lib/actions/user/service.ts
@@ -1,16 +1,16 @@
-import dbConnect from "../../db";
+import { connect } from "../../db";
 import User from "../../models/user/model";
 import { UserI } from "./interfaces";
 
 export async function createUser(user: UserI) {
-  await dbConnect();
+  await connect();
   const newUser = await User.create({ name: user.name, email: user.email });
   console.log(newUser.name, "created successfully");
   return user;
 }
 
 export async function getUser(queryObj: Partial<UserI>) {
-  await dbConnect();
+  await connect();
   const user = await User.findOne(queryObj);
   console.log(user?.name, "found");
   return user;
