4375b2709ecffa2cf8ceeb356a3050f43aab6a37
Cross-companion <mickdaniels101@gmail.com>
2025-01-15 21:09:42 +0100

    Number request page 4

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Viewport } from "next";
 import BusinessTypes from "./_features/BusinessTypes/components/BusinessTypes";
 import { CreateUserAction } from "./_lib/actions/user/actions";
+import Link from "next/link";
 
 export const viewport: Viewport = {
   themeColor: "#ffffff",
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-[3rem] tracking-[1.5rem] text-phthaloGreen font-dune">
       <BusinessTypes />
+      <Link href={"/auth/hey-number"}>Link to hey-number</Link>
       <form className="flex items-center flex-col" action={CreateUserAction}>
         <label>Name</label>
         <input
