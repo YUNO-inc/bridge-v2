9e2bc68a110ea392ac4a3955ca6d0d98adad2de4
Cross-companion <mickdaniels101@gmail.com>
2025-01-20 12:03:31 +0100

    Activity Search 1

--- app/_features/App/ActivitySearch/ActivitySearch.tsx
+++ app/_features/App/ActivitySearch/ActivitySearch.tsx
@@ -1,7 +1,10 @@
 function ActivitySearch() {
   return (
-    <div className="flex justify-end mt-4 bg-phthaloGreen bg-opacity-[0.1] w-full h-30 rounded-[42px] p-[10px]">
-      <div className="flex gap-2">
+    <div className="flex gap-1 justify-end mt-4 bg-phthaloGreen bg-opacity-[0.1] w-full h-30 rounded-[42px] p-[10px]">
+      <div className=" grow bg-phthaloGreen bg-opacity-[0.1] flex items-center">
+        <input className="outline-none w-full bg-transparent" />
+      </div>
+      <div className="flex gap-[6px]">
         <div className="w-8 h-8 bg-black bg-opacity-[0.1] rounded-full"></div>
         <div className="w-8 h-8 bg-black rounded-full"></div>
       </div>
