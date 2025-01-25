4555da6715acc931619a6d9dba450933b47d5152
Cross-companion <mickdaniels101@gmail.com>
2025-01-25 15:52:30 +0100

    Rough Business type set 2.

--- app/_features/BusinessTypes/BusinessTypes.tsx
+++ app/_features/BusinessTypes/BusinessTypes.tsx
@@ -1,15 +1,12 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { motion, useMotionValueEvent, useScroll } from "motion/react";
 import LocalIcons from "@/app/_utils/LocalIcons";
 
 export default function BusinessTypes() {
-  const carouselRef = useRef(null);
-  const { scrollX, scrollXProgress } = useScroll({ container: carouselRef });
-
-  useMotionValueEvent(scrollX, "change", (latest) => {
-    console.log("Page scroll: ", latest);
-  });
+  const carouselRef = useRef<HTMLDivElement | null>(null);
+  const { scrollX } = useScroll({ container: carouselRef });
+  const [currIndex, setCurrIndex] = useState(0);
   const businessTypes = [
     "shawarma",
     "shawarma",
@@ -33,90 +30,81 @@ export default function BusinessTypes() {
     "shawarma",
     "shawarma",
     "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
-    "shawarma",
   ];
-  const activeIndex = 0;
-  console.log(businessTypes.length);
+  const widthOfItem = 40;
+
+  useMotionValueEvent(scrollX, "change", (latest) => {
+    const currentScrollIndex = Math.round(latest / widthOfItem);
+    setCurrIndex(currentScrollIndex);
+  });
+
+  const handleSnapScroll = (index: number) => {
+    const container = carouselRef.current;
+    if (!container) return;
+    container.scrollTo({
+      left: index * widthOfItem,
+      behavior: "smooth",
+    });
+    setCurrIndex(index);
+  };
 
   return (
-    <div className="relative flex justify-center items-center bg-app-red-500 bg-opacity-10 h-[50px] w-[70%] rounded-full">
-      <div ref={carouselRef} className="flex items-center h-full overflow-auto">
-        {businessTypes.map((type, i) => {
-          return (
-            <div
-              className={`flex justify-center items-center ${
-                i === activeIndex ? "w-8" : "w-8"
-              }`}
-              key={i}
-            >
-              <LocalIcons
-                name={type}
-                pathClassName={["fill-app-red"]}
-                className="h-5"
-              />
-            </div>
-          );
-        })}
-      </div>
-      <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] h-[80%] w-10 bg-transparent border-[6px] border-x-transparent border-x-0 border-y-app-red-500 border-opacity-[0.37]"></div>
+    <div className="relative flex justify-center items-center bg-phthaloGreen-800 bg-opacity-10 w-[70%] rounded-full overflow-hidden border border-phthaloGreen-800 border-opacity-[0.37]">
+      <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] h-[90%] w-[50px] bg-transparent border-[6px] border-x-transparent border-x-0 border-y-phthaloGreen-800 border-opacity-[0.5]"></div>
+      <motion.div
+        ref={carouselRef}
+        className={`relative flex items-center h-6 overflow-auto scrollbar-hide px-[calc(50%-35px)] my-[14px]`}
+        style={{
+          scrollSnapType: "x mandatory",
+          WebkitOverflowScrolling: "touch",
+        }}
+      >
+        {businessTypes.map((type, i) => (
+          <BusinessItem
+            key={i}
+            type={type}
+            isActive={i === currIndex}
+            index={i}
+            handleClick={handleSnapScroll}
+          />
+        ))}
+      </motion.div>
     </div>
   );
+}
+
+function BusinessItem({
+  type = "",
+  isActive = false,
+  handleClick,
+  index,
+}: {
+  type: string;
+  isActive: boolean;
+  index: number;
+  handleClick: (index: number) => void;
+}) {
+  return (
+    <div
+      className={`relative flex justify-center items-center transition-all ease-out ${
+        isActive ? "min-w-[70px]" : "min-w-[40px]"
+      }`}
+      onClick={() => handleClick(index)}
+      style={{
+        scrollSnapAlign: "center",
+      }}
+    >
+      <LocalIcons
+        name={type}
+        pathClassName={[`transition-all ease-out fill-phthaloGreen-800`]}
+        className={`w-5 transition-all ease-out ${
+          isActive ? "w-6" : "opacity-[0.37]"
+        }`}
+      />
+    </div>
+  );
+}
+
+{
+  /* <a target="_blank" href="https://icons8.com/icon/24461/rice-bowl">Rice Bowl</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */
 }
