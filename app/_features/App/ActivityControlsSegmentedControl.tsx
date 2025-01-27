e907bf97bb0360a795fd9aa05c9b8511d6133a06
Cross-companion <mickdaniels101@gmail.com>
2025-01-27 12:56:50 +0100

    Fixed using redirect on client comp.

--- app/_features/App/ActivityControlsSegmentedControl.tsx
+++ app/_features/App/ActivityControlsSegmentedControl.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import SegmentedControl from "../SegmentedControl/SegmentedControl";
 import { useState } from "react";
 
 function ActivityControlsSegmentedControl() {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   function handleClick(pageName: string) {
     if (pageName === "signin") {
       setIsLoading(true);
-      redirect("/auth");
+      router.push("/auth");
     } else setIsLoading(false);
   }
 
