90cb20b070e4567b7b957b1610535d2a27079939
Cross-companion <mickdaniels101@gmail.com>
2025-01-27 15:47:49 +0100

    Basic Activity Container Structure.

--- app/_features/App/ActivityContainer/ActivityContainer.tsx
+++ app/_features/App/ActivityContainer/ActivityContainer.tsx
@@ -1,6 +1,52 @@
-function ActivityContainer() {
+"use client";
+
+import { motion, useMotionValue } from "motion/react";
+import { useState } from "react";
+import PageControl from "../../PageControl/PageControl";
+
+function ActivityContainer({ currPageIndex = 0 }) {
+  const MAX_PAGE_INDEX = 1;
+  const DRAG_BUFFER = 33;
+  const [pageIndex, setPageIndex] = useState(currPageIndex);
+  const dragX = useMotionValue(0);
+
+  const onDragEnd = () => {
+    const x = dragX.get();
+    if (x <= -DRAG_BUFFER && pageIndex < MAX_PAGE_INDEX) {
+      setPageIndex((pv) => pv + 1);
+    } else if (x >= DRAG_BUFFER && pageIndex > 0) {
+      setPageIndex((pv) => pv - 1);
+    }
+  };
+
   return (
-    <div className="bg-[#f2f2f2] h-[400px] max-h-[63.7dvh] min-h-72 w-full rounded-[25px]"></div>
+    <div className="w-full">
+      <div className="relative w-full flex justify-center items-center overflow-hidden">
+        <motion.div
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          animate={{
+            translateX: `calc((${pageIndex} * -100%) - ${
+              pageIndex > 0 ? "16px" : "0px"
+            })`,
+          }}
+          transition={{ type: "spring", mass: 3, stiffness: 400, damping: 50 }}
+          onDragEnd={onDragEnd}
+          style={{ x: dragX }}
+          className="flex w-full gap-4 h-[400px] max-h-[63.7dvh] min-h-72 active:cursor-grabbing"
+        >
+          <div className="bg-[#f2f2f2] h-full shrink-0 w-full rounded-[25px]"></div>
+          <div className="bg-[#f2f2f2] h-full shrink-0 w-full rounded-[25px]"></div>
+        </motion.div>
+        <div className="absolute left-1/2 translate-x-[-50%] bottom-1 z-10">
+          <PageControl
+            numOptions={2}
+            activeIndex={pageIndex}
+            setActiveIndex={setPageIndex}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
--- app/_features/PageControl/PageControl.tsx
+++ app/_features/PageControl/PageControl.tsx
@@ -4,9 +4,23 @@ import { anonymousPro } from "@/app/fonts";
 
 type PageControlProps = {
   text?: string;
+  numOptions?: number;
+  activeIndex?: number;
+  setActiveIndex?: (dotIndex: number) => void; //This can be an array also if multiple functionalities are needed.
 };
 
-export default function PageControl({ text }: PageControlProps) {
+export default function PageControl({
+  text,
+  numOptions = 2,
+  activeIndex = 1,
+  setActiveIndex,
+}: PageControlProps) {
+  function handleDotClick(dotIndex: number) {
+    if (!setActiveIndex) return;
+
+    setActiveIndex(dotIndex);
+  }
+
   if (text)
     return (
       <div className="bg-grey1 px-3 py-2 max-h-6 bg-opacity-[44%] rounded-full flex items-center justify-center backdrop-blur-[50px]">
@@ -19,8 +33,16 @@ export default function PageControl({ text }: PageControlProps) {
     );
 
   return (
-    <div className="bg-grey1 px-3 py-2 max-h-6 bg-opacity-[44%] rounded-full flex items-center justify-center">
-      dots
+    <div className="bg-grey1 px-3 py-2 max-h-6 bg-opacity-[44%] rounded-full flex items-center justify-center gap-2">
+      {Array.from({ length: numOptions }, (_, index) => (
+        <button
+          key={index}
+          className={`w-2 h-2 bg-black rounded-full transition-all duration-300 ${
+            activeIndex === index ? "bg-opacity-100" : "bg-opacity-30"
+          }`}
+          onClick={() => handleDotClick(index)}
+        ></button>
+      ))}
     </div>
   );
 }
