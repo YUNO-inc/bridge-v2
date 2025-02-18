e3cc20788b05fe27f361a03b4d4277007862cddd
Cross-companion <mickdaniels101@gmail.com>
2025-02-18 22:08:57 +0100

    Map v1.9.1

--- app/address/new/page.tsx
+++ app/address/new/page.tsx
@@ -4,7 +4,7 @@ import Map from "@/app/_features/Map/AddressMap";
 function Page() {
   return (
     <div className="flex flex-col min-h-[100svh] bg-black-50">
-      <PageBackLink className="absolute top-0 left-0 z-[9999] shadow-sgc rounded-full m-4 p-0.5 bg-black-100 text-white text-opacity-60" />
+      <PageBackLink className="absolute top-0 left-0 z-[9999] shadow-sgc rounded-full m-4 p-0.5 bg-white text-opacity-60" />
       <Map />
     </div>
   );
