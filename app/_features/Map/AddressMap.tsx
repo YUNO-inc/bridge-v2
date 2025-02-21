2284c2fcc99525bd90fd77ac206ff87e2416e8f4
Cross-companion <mickdaniels101@gmail.com>
2025-02-21 23:46:09 +0100

    Map v1.10.5 viewport test - 1

--- app/_features/Map/AddressMap.tsx
+++ app/_features/Map/AddressMap.tsx
@@ -2,17 +2,27 @@
 
 import dynamic from "next/dynamic";
 import FlexibleModal from "../Modals/FlexibleModal";
+import { useEffect } from "react";
 
 const Container = dynamic(() => import("./AddressMapContainer"), {
   ssr: false,
 });
 
 function Map() {
+  useEffect(() => {
+    // Prevent scrolling when map is full viewport
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, []);
+
   return (
-    <>
-      <Container className="w-full h-[78svh] sm:h-svh sm:w-[83vw]" />
-      <FlexibleModal className="h-[40svh] w-svw sm:h-svh sm:w-[20vw] sm:rounded-l-2xl sm:rounded-r-none" />
-    </>
+    <div className="full-viewport">
+      <Container className="w-full h-[78dvh] sm:h-dvh sm:w-[83vw]" />
+      <FlexibleModal className="h-[40dvh] w-svw min-w-min sm:h-dvh sm:w-[20vw] sm:rounded-l-2xl sm:rounded-r-none" />
+    </div>
   );
 }
 
