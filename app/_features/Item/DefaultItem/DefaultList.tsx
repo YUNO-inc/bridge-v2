761856c1712fe91ae927bf631d0cc751c5adb815
Cross-companion <mickdaniels101@gmail.com>
2025-04-22 17:28:50 +0100

    Business and Items Data Setup 2.10

--- app/_features/Item/DefaultItem/DefaultList.tsx
+++ app/_features/Item/DefaultItem/DefaultList.tsx
@@ -1,6 +1,5 @@
 import { auth } from "@/app/_lib/actions/auth/auth";
 import DefaultItem from "./DefaultItem";
-import { GetNearBusinessesAction } from "@/app/_lib/actions/business/actions";
 import { BusinessDTO, BusinessTypesDTO } from "@/app/_interfaces/interfaces";
 
 async function DefaultList({
@@ -15,11 +14,13 @@ async function DefaultList({
     selectedAddress =
       user.addresses.find((a) => a.isSelected) || user.addresses[0];
   }
+  const [lon, lat] = selectedAddress?.coordinates || [];
 
-  const businesses = await GetNearBusinessesAction(
-    selectedAddress?.coordinates,
-    { businessTypes: [businessType] }
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/business?lon=${lon}&loat=${lat}&businessType=${businessType}`
   );
+
+  const businesses: BusinessDTO[] = await res.json();
 
   return (
     <div className="p-2 flex flex-col gap-4">
---
+++ app/api/business/route.ts
@@ -0,0 +1,28 @@
+import {
+  BUSINESS_TYPES,
+  BusinessTypesDTO,
+  DEFAULT_COORDS,
+} from "@/app/_interfaces/interfaces";
+import { getNearBusinesses } from "@/app/_lib/actions/business/service";
+import { NextRequest, NextResponse } from "next/server";
+
+function isBusinessTypesDTO(type: string): type is BusinessTypesDTO {
+  return BUSINESS_TYPES.includes(type as BusinessTypesDTO);
+}
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const lon = Number(searchParams.get("lon")) || undefined;
+  const lat = Number(searchParams.get("lat")) || undefined;
+  const businessTypes: BusinessTypesDTO[] = searchParams
+    .getAll("businessType")
+    .filter((type): type is BusinessTypesDTO => isBusinessTypesDTO(type));
+  const coords: [number, number] =
+    typeof lon === "number" && typeof lat === "number"
+      ? [lon, lat]
+      : DEFAULT_COORDS;
+
+  const businesses = await getNearBusinesses(coords, { businessTypes });
+
+  return NextResponse.json(businesses);
+}
