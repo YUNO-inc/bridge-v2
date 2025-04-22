babb01f8ac8bf4df8f5bb1986e2c07dd828fc108
Cross-companion <mickdaniels101@gmail.com>
2025-04-22 21:51:00 +0100

    Business and Items Data Setup 2.13

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Viewport } from "next";
-import ActivityControls from "@/app/_features/App/ActivityControls";
 import ProfileImage from "./_features/ProfileImage/ProfileImage";
 
 export const viewport: Viewport = {
@@ -10,7 +9,6 @@ export default async function Home() {
   return (
     <div className="relative flex flex-col min-h-[100svh]">
       <ProfileImage />
-      <ActivityControls />
     </div>
   );
 }
