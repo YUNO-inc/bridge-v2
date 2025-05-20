692f8eab764d5ff4b5485e8eec8f2ceee0e9a717
Cross-companion <mickdaniels101@gmail.com>
2025-05-20 14:08:46 +0100

    Deliver 2.2

--- app/app/[businessSlug]/page.tsx
+++ app/app/[businessSlug]/page.tsx
@@ -1,8 +1,7 @@
 async function Page() {
   return (
     <p className="text-stone-700 text-opacity-[0.67] text-sm py-2 px-2.5">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur labore
-      molestiae suscipit mollitia sint reiciendis expedita?
+      {""}
     </p>
   );
 }
