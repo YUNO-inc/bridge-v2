5c8adbed2c1da8585e71fa8eb1c6b552e68fe353
Cross-companion <mickdaniels101@gmail.com>
2025-07-16 11:28:18 +0100

    Referral 6.2

--- app/_features/Referral/OnlyRefLink.tsx
+++ app/_features/Referral/OnlyRefLink.tsx
@@ -3,6 +3,7 @@
 import { copyToClipboard } from "@/app/_utils/copyToClipboard";
 
 function OnlyRefLink({ url }: { url: string }) {
+  console.log(url)
   return (
     <button
       className="active:underline transition pt-1 relative -bottom-2 text-phthaloGreen text-opacity-[0.37]"
